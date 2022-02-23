bce76e33db96ff1adeef7ebf61a7d38ca74c7fff
wilyyy <alvarez_william@outlook.com>
2022-02-22 23:31:48 -0800

    smol stuff

--- Model/anime.js
+++ Model/anime.js
@@ -1,1 +1,11 @@
-// schema and schema model defined here
+// schema and schema model defined hereconst express = require('express');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const animesSchema = new Schema({
+
+})
+
+const Animes = mongoose.model('Animes', animesSchema);
+
+module.exports = Animes;
--- Routes/anime.js
+++ Routes/anime.js
@@ -1,1 +1,6 @@
 //import crud functions defined from controller and use express router to connect it to route
+const express = require('express');
+const { append } = require('express/lib/response');
+const router = express.Router();
+
+module.exports = router;
